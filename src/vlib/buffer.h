3abc809cc18272b13a0183fb17fabd29579954ac
Michal Mazur <mkm@semihalf.com>
2017-12-05 15:18:08 +0100

    vlib: Add pointer to driver data to vlib_buffer_t
    Change-Id: I97617d84eb5ba6403bc2d8f87d8c76a086413094
    Signed-off-by: Michal Mazur <mkm@semihalf.com>

--- src/vlib/buffer.h
+++ src/vlib/buffer.h
@@ -137,7 +137,8 @@ typedef struct
                       if VLIB_PACKET_IS_TRACED flag is set.
                    */
   u32 recycle_count; /**< Used by L2 path recycle code */
-  u32 opaque2[14];  /**< More opaque data, currently unused */
+  void *l2_priv_data; /**< L2 node private data pointer */
+  u32 opaque2[12]; /**< More opaque data, currently unused */
 
   /***** end of second cache line */
     CLIB_CACHE_LINE_ALIGN_MARK (cacheline2);
